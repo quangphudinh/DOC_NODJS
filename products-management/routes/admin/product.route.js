cc1f3dc08572f6f01fd99760910801d3b1798c1e
John Doe <johndoe@email.com>
2024-09-02 21:18:31 +0700

    Trang detail

--- products-management/routes/admin/product.route.js
+++ products-management/routes/admin/product.route.js
@@ -20,13 +20,12 @@ router.delete('/delete/:id', controller.deleteItem);
 router.get('/create', controller.create);
 
 // router.post('/create', controller.createPost); // khong su dung upload
-router.post('/create', upload.single('thumbnail'), 
-    validate.createPost, controller.createPost);
+router.post('/create', upload.single('thumbnail'), validate.createPost, controller.createPost);
 
 router.get('/edit/:id', controller.edit);
 
-router.patch('/edit/:id',  upload.single('thumbnail'), 
-validate.createPost, controller.editPatch);
+router.patch('/edit/:id',  upload.single('thumbnail'), validate.createPost, controller.editPatch);
 
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
